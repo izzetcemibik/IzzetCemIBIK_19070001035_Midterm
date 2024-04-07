2972ce713e075755f97cd6483eabb222d111197d
İzzet Cem İbik <izzetcem2001@gmail.com>
2024-04-07 11:37:26 +0300

    homepage added

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 import mysql.connector
+from flask import Flask, render_template
 
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="12345Izo",
-    database="izzetcemibik_19070001035_midtermdb"
-)
+app = Flask(__name__)
+
+def connect_to_mysql():
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="12345Izo",
+            database="izzetcemibik_19070001035_midtermdb"
+        )
+        if conn.is_connected():
+            print('Connected to MySQL database')
+        return conn
+
+    except mysql.connector.Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+        return None
+
+@app.route('/')
+def home():
+    conn = connect_to_mysql()
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
